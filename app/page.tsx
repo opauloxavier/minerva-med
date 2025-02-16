71093d633a6590ad432adcb1241c44ba37462c12
Paulo Xavier <contato.pauloxavier@gmail.com>
2025-02-16 12:32:30 -0300

    Clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Home from "./src/components/home";
+
 config.autoAddCss = false; // Prevent Font Awesome from adding its own CSS
 
 export default function Page() {

cc7dad0f5627989845d381b22cbc63e1b9da4c8f
Paulo Xavier <contato.pauloxavier@gmail.com>
2025-02-26 19:44:53 -0300

    Update hotsite with logo and CTA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
@@ -15,9 +16,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <Analytics />
-      <SpeedInsights />
-      <body className={`antialiased`}>{children}</body>
+      <Head>
+        <title>Minerva Medicina Personalizada</title>
+        <meta
+          name="description"
+          content="Minerva Medicina Personalizada - Medicina de Precisão. Em breve."
+        />
+        <meta
+          name="keywords"
+          content="medicina personalizada, medicina de precisão, saúde, clínica"
+        />
+        <meta name="author" content="Minerva Medicina Personalizada" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+      <body className={`antialiased`}>
+        <Analytics />
+        <SpeedInsights />
+        {children}
+      </body>
     </html>
   );
 }
